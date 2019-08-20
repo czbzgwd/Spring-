ed8cb2327f1e8337c79941ce70e303a76843fb43
tom_plb <goodMorning_glb@atguigu.com>
2019-08-20 23:09:00 +0800

    27、AOP-AOP功能测试

---
+++ src/test/java/com/atguigu/test/IOCTest_AOP.java
@@ -0,0 +1,30 @@
+package com.atguigu.test;
+
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.atguigu.aop.MathCalculator;
+import com.atguigu.bean.Boss;
+import com.atguigu.bean.Car;
+import com.atguigu.bean.Color;
+import com.atguigu.bean.Red;
+import com.atguigu.config.MainConfigOfAOP;
+import com.atguigu.dao.BookDao;
+
+public class IOCTest_AOP {
+	
+	@Test
+	public void test01(){
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfigOfAOP.class);
+		
+		//1、不要自己创建对象
+//		MathCalculator mathCalculator = new MathCalculator();
+//		mathCalculator.div(1, 1);
+		MathCalculator mathCalculator = applicationContext.getBean(MathCalculator.class);
+		
+		mathCalculator.div(1, 1);
+		
+		applicationContext.close();
+	}
+
+}
